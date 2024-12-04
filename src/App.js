8723cf9976cd5a104ba4d48e085b204a83054c57
umarfarooq8980 <mfarooqalizai@gmail.com>
2024-12-04 14:18:18 +0500

    dashboard1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import './Styles/app.css'; // Import global styles
-import Navbar from './components/navbar';
-import Landingpage from './components/landingpage';  
+import React from "react";
+import UserInfo from "./components/UserInfo";
+import Statistics from "./components/Statistics";
+import Messages from "./components/Messages";
+
 function App() {
-return (
-<div>
-<Navbar />
-<Landingpage />
-</div>
-);
+
+  const userData = {
+    name: "Muhammad Umar Farooq",
+    email: "24g-bcs784@student.agu.edu.pk",
+    role: "Student",
+    profilePicture: "https://miu.edu.ng/wp-content/uploads/2021/06/AdobeStock_113096046.jpg", 
+  };
+
+  // Statistics Data Object
+  const statistics = {
+    totalUsers: 100,
+    activeUsers: 75,
+    newSignups: 15,
+    activePercentage: ((45 / 54) * 100).toFixed(2), 
+  };
+
+  
+  const messages = [
+    "Welcome to GU TECH Karachi!",
+    "We are are available.",
+    "Thank You and Enjoy!",
+  ];
+
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Dashboard</h1>
+      <UserInfo userData={userData} />
+      <Statistics statistics={statistics} />
+      <Messages messages={messages} />
+    </div>
+  );
 }
+
 export default App;

70327445c9318a59eded0a525adedd7c76b70fd3
umarfarooq8980 <mfarooqalizai@gmail.com>
2024-12-04 13:56:46 +0500

    user info components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 import React from "react";
 import UserInfo from "./components/UserInfo";
 import Statistics from "./components/Statistics";
+import Messages from "./components/Messages";
 
 function App() {
-  // User Data Object
+  
   const userData = {
-    name: "Muhammad Umar Farooq ",
-    email: "umaraalizai@gmail.com",
-    role: "softwear engineer",
-    profilePicture: "https://www.mediaan.com/wp-content/uploads/2020/08/Software-Engineer-Blog-Featured-Image.jpg", 
+    name: "Muhammad Umar Farooq",
+    email: "mfarooqalizai@gmail.com",
+    role: "Student",
+    profilePicture: "https://miu.edu.ng/wp-content/uploads/2021/06/AdobeStock_113096046.jpg", // Placeholder for profile image
   };
 
   
   const statistics = {
-    totalUsers: 54,
-    activeUsers: 45,
-    newSignups: 10,
+    totalUsers: 100,
+    activeUsers: 65,
+    newSignups: 15,
     activePercentage: ((45 / 54) * 100).toFixed(2), 
   };
+
+ 
+  const messages = [
+    "Welcome To The GU TECH Karachi!",
+    "Now we are Available.",
+    "Good and Enjoy!",
+  ];
 
   return (
     <div style={{ padding: "20px", fontFamily: "Arial" }}>
       <h1>Dashboard</h1>
       <UserInfo userData={userData} />
       <Statistics statistics={statistics} />
+      <Messages messages={messages} />
     </div>
   );
 }
 
 export default App;
-
